9d67db24f0608871e9112b22cffadfce98524345
chmpgnsupernova <164203740+chmpgnsupernova@users.noreply.github.com>
2024-11-12 11:08:16 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
-$host = 'localhost';
-$db = 'todo';
-$user = 'root';
-$pass = '';
+$host = '203.175.9.20';
+$db = 'todh8447_todo';           
+$user = 'root';      
+$pass = '';        
 
+// Create a connection
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully!";
 }
 ?>
